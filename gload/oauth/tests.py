982e1760d90282786bb834c2bc246953b21cc33d
Kostya <run06mail.ru@gmail.com>
2024-03-03 13:42:15 +0300

    4 commit

--- gload/oauth/tests.py
+++ gload/oauth/tests.py
@@ -5,33 +5,62 @@ import re
 
 
 class AuthTests(APITestCase):
-
     def setUp(self):
         super().setUp()
+        self.user_email = 'test@example.com'
+        self.user_password = 'password0123'
+        self.invalid_email = 'test@examplsde.com'
+
+    def register_user(self):
         url = reverse('customuser-list')
-        response = self.client.post(url, {'email': 'test@example.com', 'password': 'password0123'}, format='json')
+        response = self.client.post(url, {'email': self.user_email, 'password': self.user_password}, format='json')
+        return response
+
+    def get_activation_details(self, email_body):
+        url_pattern = re.compile(r'http://testserver/activate/\w+/\w+-\w+')
+        match = url_pattern.search(email_body)
+        activation_url = match.group()
+        activation_url = activation_url[27:]
+        uid = activation_url[0:2]
+        token = activation_url[3:]
+        return uid, token
+
+    def activate(self):
+        url = reverse('customuser-activation')
+        res = self.register_user()
+        email = mail.outbox[0]
+        uid,token = self.get_activation_details(email.body)
+        response = self.client.post(url, {'uid': uid, 'token': token}, format='json')
+        return response
+
+    def test_user_registration(self):
+        response = self.register_user()
         self.assertEqual(response.status_code, 201)
         self.assertEqual(len(mail.outbox), 1)
         email = mail.outbox[0]
-        self.assertEqual(email.to, ['test@example.com'])
-
-        email_body = email.body
-        url_pattern = re.compile(r'http://testserver/activate/\w+/\w+-\w+')
-
-        # Поиск URL в тексте письма
-        match = url_pattern.search(email_body)
-
-        activation_url = match.group()
-
-        activation_url = activation_url[27:]
-
-        uid = activation_url[0:2]
-        token = activation_url[3:]
-
-        self.uid = uid
-        self.token = token
+        self.assertEqual(email.to, [self.user_email])
+        uid, token = self.get_activation_details(email.body)
+        self.assertIsNotNone(uid)
+        self.assertIsNotNone(token)
 
     def test_activation(self):
+        self.register_user()
+        email_body = mail.outbox[0].body
+        uid, token = self.get_activation_details(email_body)
         url = reverse('customuser-activation')
-        response = self.client.post(url,{'uid':self.uid,'token': self.token}, format = 'json')
-        self.assertEqual(response.status_code,204)
+        response = self.client.post(url, {'uid': uid, 'token': token}, format='json')
+        self.assertEqual(response.status_code, 204)
+
+    def login(self):
+        self.register_user()
+        self.activate()
+        url = reverse('login')
+        response1 = self.client.post(url, {'email': self.user_email, 'password': self.user_password}, format='json')
+        response2 = self.client.post(url, {'email': self.invalid_email, 'password': self.user_password}, format='json')
+        self.assertEqual(response1.status_code, 200)
+        self.assertEqual(response2.status_code, 400)
+
+
+
+
+
