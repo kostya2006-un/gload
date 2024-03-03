f9b1c642020ac43f08df2a0f6131b225aa58b98f
Kostya <run06mail.ru@gmail.com>
2024-03-03 20:53:36 +0300

    4 commit

--- gload/music/models.py
+++ gload/music/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    release_date = models.DateField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
--- gload/music/tests.py
+++ gload/music/tests.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class MyApiTests(APITestCase):
 
     def setUp(self):
